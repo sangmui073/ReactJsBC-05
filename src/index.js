8e0182f261a177386f2eb92ad7468a6839ff1b3b
TM-Phuc <sangmui073@gmail.com>
2021-04-09 20:43:25 +0700

    add code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/jquery/dist/jquery.slim";
+import "../node_modules/popper.js/dist/popper";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+
+//setup bootstrap
 ReactDOM.render(
   <React.StrictMode>
     <App />
